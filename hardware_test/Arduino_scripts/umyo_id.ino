8acc61f2b905b084775abeb5f3cfef0f7b5931f0
MadNad <varzonova.nadezhda@gmail.com>
2024-12-02 10:51:06 +0100

    Added hardware test scripts we used to plot the initial data.

---
+++ hardware_test/Arduino_scripts/umyo_id.ino
@@ -0,0 +1,30 @@
+/*
+Obtaining muscle data from uMyo via BLE on various Arduino boards
+Usage: install ArduinoBLE library, run the code on BLE enabled Arduino (nRF52 or ESP32 core)
+ - open Serial Plotter (or Serial Monitor) at 115200 speed
+ - turn on uMyo device
+ - switch it into BLE mode if necessary (to switch, press button once or twice, depending on current mode)
+ - you should see muscle activity chart on a plot (or as serial output)
+*/
+
+#include <uMyo_BLE.h>
+
+void setup() {
+  Serial.begin(115200);
+  uMyo.begin();
+}
+
+void loop() 
+{
+  uMyo.run();
+  int dev_count = uMyo.getDeviceCount(); //if more than one device is present, show all of them
+  for(int d = 0; d < dev_count; d++)
+  {
+    Serial.print("ID of device[");
+    Serial.print(d);
+    Serial.print("]: ");
+    Serial.print(uMyo.getID(d));
+    Serial.println();
+  }
+  delay(30);
+}
